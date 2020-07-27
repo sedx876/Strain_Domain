a37b71a766969c8e2b2bd39dbf4a4f0d147e8df3
sedx876 <swatkins876@gmail.com>
2020-07-27 08:52:35 -0400

    Adapters and GET fetches

--- Frontend/src/listAdapter.js
+++ Frontend/src/listAdapter.js
@@ -1,5 +1,10 @@
 class ListAdapter {
   constructor() {
-    
+    this.baseURL = "http://localhost:3000/lists"
+  }
+
+  getLists() {
+    return fetch(this.baseURL)
+    .then(res => res.json())
   }
 }
--- Frontend/src/strainAdapter.js
+++ Frontend/src/strainAdapter.js
@@ -1,5 +1,10 @@
 class StrainAdapter {
   constructor() {
-    
+    this.baseURL = "http://localhost:3000/strains"
+  }
+
+  getStrains() {
+    return fetch(this.baseURL)
+    .then(res => res.json())
   }
 }
