0cc857522265cbbfe438fcc1c0b887257444e257
sedx876 <swatkins876@gmail.com>
2020-07-27 07:55:50 -0400

    controllers setup

--- Backend/app/controllers/lists_controller.rb
+++ Backend/app/controllers/lists_controller.rb
@@ -1,2 +1,23 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :update, :destroy]
+
+  def index
+    @lists = List.all 
+    render json: ListSerializer.new(@lists), status: 200
+  end
+
+  def show
+    render json: ListSerializer.new(@list), status: 200
+  end
+
+
+private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:title)
+  end
 end
--- Backend/app/controllers/strains_controller.rb
+++ Backend/app/controllers/strains_controller.rb
@@ -1,2 +1,38 @@
 class StrainsController < ApplicationController
+  before_action :set_strain, only: [:show, :update, :destroy]
+
+  def index
+    @strains = Strain.all 
+    render json: StrainSerializer.new(@strains), status: 200
+  end
+
+  def show
+    render json: StrainSerializer.new(@strain), status: 200
+  end
+
+  def create
+    @strain = Strain.new(strain_params)
+    if @strain.save
+      render json: StrainSerializer.new(@strain), status: 200
+    else
+      render json: @strain.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @strain = Strain.find(params[:id])
+    @strain.destroy
+  end
+
+  private
+
+  def set_strain
+    @strain = Strain.find(params[:id])
+  end
+
+  
+  def strain_params
+    params.require(:strain).permit(:name, :notes, :list_id)
+  end
+
 end
