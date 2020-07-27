9b4c814a18ef8b5181cacd55795f564e3a18f30d
sedx876 <swatkins876@gmail.com>
2020-07-27 10:03:46 -0400

    strain constructor and renderCard

--- Frontend/src/strain.js
+++ Frontend/src/strain.js
@@ -1,5 +1,25 @@
-class Strain {
-  constructor() {
-    
+class Strain{
+  constructor(strainJSON) {
+    this.name = strainJSON.attributes.name
+    this.notes = strainJSON.attributes.notes
+    this.id = strainJSON.id
+    this.listTitle = strainJSON.attributes.list.title
+    this.listId = strainJSON.attributes.list.id
+  }
+
+  renderCard(){
+      return `
+      <div class="card bg-success text-white" id="card-${this.id}">
+      <div class="card-header">
+      <h3>${this.listTitle}</h3>
+      </div>
+      <div class="card-body">
+      <h5>${this.name}</h5>
+      <p>${this.notes}</p>
+      <button type="submit" class="btn btn-danger btn-small" 
+      id="${this.id}">Delete This Strain</button>
+      </div>
+      </div>
+      `;
   }
 }
