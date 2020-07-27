dd876f3a287bf8effc50aed762b0c3f3ca60dc4f
sedx876 <swatkins876@gmail.com>
2020-07-27 10:30:38 -0400

    strains eventlisteners

--- Frontend/src/strains.js
+++ Frontend/src/strains.js
@@ -1,5 +1,18 @@
 class Strains {
   constructor() {
-    
+    this.strains = [];
+    this.adapter = new StrainAdapter();
+    this.fetchAndLoadStrains();
+    this.initBindingsAndEventListeners();
+  }
+
+  initBindingsAndEventListeners() {
+    this.strainContainer = document.getElementById("savedStrains-container");
+    this.newStrainName = document.querySelector(".form-group #strainName");
+    this.newStrainNotes = document.querySelector(".form-group #strainNotes");
+    this.dropdownContainer = document.querySelector(".form-control #ListControlSelect");
+    this.cardForm = document.getElementById("strainForm");
+    this.cardForm.addEventListener("submit", this.createCard.bind(this));
+    this.strainContainer.addEventListener("click", e => this.deleteCard(e.target.id));
   }
 }
