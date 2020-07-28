e4d36081c93eb64aba53fc458cb78951c4223804
sedx876 <swatkins876@gmail.com>
2020-07-28 10:58:45 -0400

    added popup modal feature

---
+++ Frontend/src/modal.js
@@ -0,0 +1,22 @@
+const modal = document.querySelector('#my-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.close');
+
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+function openModal() {
+  modal.style.display = 'block';
+}
+
+function closeModal() {
+  modal.style.display = 'none';
+  closeBtn.style.color = '#000000';
+}
+
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
